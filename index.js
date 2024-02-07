23fe8d3703f7f6b4101ded928cde434ffe1a7108
nftpixels <97366705+nftpixels@users.noreply.github.com>
2024-02-07 23:22:36 +1300

    Update index.js

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ app.post('/assignRole', async (req, res) => {
     try {
         const { userId } = req.body;
 
-        console.log('Received Role Assignmentclear Request For: ' + userId);
+        console.log('Received Role Assignment Request For: ' + userId);
 
         // Your logic to assign a custom role to the user based on the userID
         const guildId = '1038740889289367592'; 
